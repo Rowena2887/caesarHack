8272f117282a43ec351e85b23b2150ec1a382d19
tchaoy <49221941+Rowena2887@users.noreply.github.com>
2020-12-17 22:10:50 -0800

    Brute force Caesar Cipher.

---
+++ caesarHacker.py
@@ -0,0 +1,28 @@
+# Caesar Cipher Hacker
+
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM'
+
+# Loop through every possible key:
+for key in range(len(SYMBOLS)):
+	translated = ''
+	
+	# Loop through each symbol in message:
+	for symbol in message:
+		if symbol in SYMBOLS:
+			symbolIndex = SYMBOLS.find(symbol)
+			translatedIndex = symbolIndex - key
+			
+			# Handle the wraparound:
+			if translatedIndex < 0:
+				translatedIndex = translatedIndex + len(SYMBOLS)
+				
+			# Append the decrypted symbol:
+			translated = translated + SYMBOLS[translatedIndex]
+		else:
+			# Append the symol without encrypting/decrypting:
+			translated = translated + symbol
+			
+	# Display every possible decryption:
+	print('Key #%s: %s'%(key, translated))
